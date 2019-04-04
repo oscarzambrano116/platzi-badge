28b1d698e5c305228700aa0e912d29e7d34f2464
Oscar Zambrano <oscarz.developer@gmail.com>
2019-04-04 18:48:45 -0300

    Api call

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -2,60 +2,53 @@ import React, { Component, Fragment } from 'react'
 import { Link } from 'react-router-dom'
 import BadgesListItem from '../components/BadgesListItem'
 import './styles/Badges.css'
-
 import confLogo from '../images/badge-header.svg'
-const DATA = [
-  {
-    id: '2de30c42-9deb-40fc-a41f-05e62b5939a7',
-    firstName: 'Freda',
-    lastName: 'Grady',
-    email: 'Leann_Berge@gmail.com',
-    jobTitle: 'Legacy Brand Director',
-    twitter: 'FredaGrady22221-7573',
-    avatarUrl: 'https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon',
-  },
-  {
-    id: 'd00d3614-101a-44ca-b6c2-0be075aeed3d',
-    firstName: 'Major',
-    lastName: 'Rodriguez',
-    email: 'Ilene66@hotmail.com',
-    jobTitle: 'Human Research Architect',
-    twitter: 'MajorRodriguez61545',
-    avatarUrl: 'https://www.gravatar.com/avatar/d57a8be8cb9219609905da25d5f3e50a?d=identicon',
-  },
-  {
-    id: '63c03386-33a2-4512-9ac1-354ad7bec5e9',
-    firstName: 'Daphney',
-    lastName: 'Torphy',
-    email: 'Ron61@hotmail.com',
-    jobTitle: 'National Markets Officer',
-    twitter: 'DaphneyTorphy96105',
-    avatarUrl: 'https://www.gravatar.com/avatar/e74e87d40e55b9ff9791c78892e55cb7?d=identicon',
-  }, 
-]
-
+import api from '../api'
 class Badges extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      data: [],
+      data: null,
+      loading: true,
+      error: null,
     }
   }
 
   componentDidMount() {
-    this.timeoutId = setTimeout(() => {
-      this.setState({
-        data: DATA,
-      })
-    }, 3000)
+    this.fetchData()
   }
 
-  componentWillUnmount() {
-    clearTimeout(this.timeoutId)
+  fetchData = async () => {
+    this.setState({
+      loading: true,
+      error: null,
+    })
+
+    try {
+      const data = await api.badges.list()
+      this.setState({
+        loading: false,
+        data,
+      })
+    } catch (error) {
+      this.setState({
+        loading: false,
+        error,
+      })
+    }
   }
 
   render() {
-    const { data } = this.state
+    const {
+      loading, 
+      data,
+      error,
+    } = this.state
+
+    if (loading) return 'Loading...'
+
+    if (error) return `Error: ${error.message}`
+
     return (
       <Fragment>
         <div className="Badges">
@@ -76,11 +69,20 @@ class Badges extends Component {
           <div className="Badges__container">
             <ul className="list-unstyled">
               {
-                data.map((badge) => (
-                  <li key={badge.id}>
-                    <BadgesListItem {...badge} />
-                  </li>
-                ))
+                data.length ? (
+                  data.map((badge) => (
+                    <li key={badge.id}>
+                      <BadgesListItem {...badge} />
+                    </li>
+                  ))
+                ) : (
+                  <div>
+                    <h3>No badges were found</h3>
+                    <Link className="btn btn-primary" to="/badge/new">
+                      Create new badges
+                    </Link>
+                  </div>
+                )
               }
             </ul>
           </div>
